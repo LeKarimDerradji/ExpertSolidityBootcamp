9b24784b38d777a31dfca809169f1e4f09c64792
LeKarimDerradji <karim.derradji.pro@gmail.com>
2022-10-05 08:27:38 +0200

    removing_unecessary_variables

--- exercises/gas/contracts/Gas.sol
+++ exercises/gas/contracts/Gas.sol
@@ -3,7 +3,6 @@ pragma solidity 0.8.0;
 
 contract GasContract {
     uint256 public immutable totalSupply; // cannot be updated
-    uint256 private immutable tradePercent;
     uint256 private paymentCounter;
 
     mapping(address => uint256) private balances;
@@ -24,65 +23,28 @@ contract GasContract {
         PaymentType paymentType;
         uint256 paymentID;
         uint256 amount;
-        bool adminUpdated;
-        string recipientName; // max 8 characters
-        address recipient;
-        address admin; // administrators address
     }
-
-    struct History {
-        uint256 lastUpdate;
-        uint256 blockNumber;
-        address updatedBy;
-    }
-
-    // Put back important struct
-
-    event AddedToWhitelist(address userAddress, uint256 tier);
 
     event Transfer(address recipient, uint256 amount);
-    event PaymentUpdated(
-        address admin,
-        uint256 ID,
-        uint256 amount,
-        string recipient
-    );
-    event WhiteListTransfer(address _recipient);
 
     constructor(address[5] memory _admins, uint256 _totalSupply) {
         contractOwner = msg.sender;
         totalSupply = _totalSupply;
         administrators = _admins;
         balances[contractOwner] = _totalSupply;
-        tradePercent = 12;
     }
 
     function balanceOf(address _user) public view returns (uint256 balance_) {
         return balances[_user];
     }
+
     // TradeFlag and dividendFlag should be bool
     function getTradingMode() external pure returns (bool) {
         return true;
     }
 
-    function addHistory(address _updateAddress)
-        internal
-        view
-        returns (bool status_, bool tradeMode_)
-    {
-        History memory history;
-        history.blockNumber = block.number;
-        history.lastUpdate = block.timestamp;
-        history.updatedBy = _updateAddress;
-        bool[] memory status = new bool[](tradePercent);
-        for (uint256 i = 0; i < tradePercent; i++) {
-            status[i] = true;
-        }
-        return ((status[0] == true), true);
-    }
-
     function getPayments(address _user)
-        internal
+        external
         view
         returns (Payment[] memory payments_)
     {
@@ -92,21 +54,13 @@ contract GasContract {
     function transfer(
         address _recipient,
         uint256 _amount,
-        string memory _name
+        string memory name
     ) external {
         balances[msg.sender] -= _amount;
         balances[_recipient] += _amount;
         emit Transfer(_recipient, _amount);
         payments[msg.sender].push(
-            Payment(
-                PaymentType.BasicPayment,
-                ++paymentCounter,
-                _amount,
-                false,
-                _name,
-                _recipient,
-                address(0)
-            )
+            Payment(PaymentType.BasicPayment, ++paymentCounter, _amount)
         );
     }
 
@@ -116,62 +70,30 @@ contract GasContract {
         uint256 _amount,
         PaymentType _type
     ) external {
-        //payment[_user].paymentID = _ID;
-        payments[_user][_ID].adminUpdated = true;
-        payments[_user][_ID].admin = _user;
         payments[_user][_ID].paymentType = _type;
         payments[_user][_ID].amount = _amount;
-        addHistory(_user);
-        emit PaymentUpdated(
-            msg.sender,
-            _ID,
-            _amount,
-            payments[_user][_ID].recipientName
+    }
+
+    function addToWhitelist(address _userAddrs, uint256 _tier) external {
+        whitelist[_userAddrs] = _tier;
+    }
+
+    function whiteTransfer(address _recipient, uint256 _amount) public {
+        require(
+            whitelist[msg.sender] > 0 || whitelist[msg.sender] < 4,
+            "Gas Contract CheckIfWhiteListed modifier : revert happened because the user is not whitelisted"
         );
+        require(
+            balances[msg.sender] >= _amount,
+            "Gas Contract - whiteTransfers function - Sender has insufficient Balance"
+        );
+        require(
+            _amount > 3,
+            "Gas Contract - whiteTransfers function - amount to send have to be bigger than 3"
+        );
+        balances[msg.sender] -= _amount;
+        balances[_recipient] += _amount;
+        balances[msg.sender] += whitelist[msg.sender];
+        balances[_recipient] -= whitelist[msg.sender];
     }
-
-
-function addToWhitelist(address _userAddrs, uint256 _tier)
-    external
-   
-{
-    require(
-        _tier < 255,
-        "Gas Contract - addToWhitelist function -  tier level should not be greater than 255"
-    );
-    whitelist[_userAddrs] = _tier;
-    if (_tier > 3) {
-        whitelist[_userAddrs] -= _tier;
-        whitelist[_userAddrs] = 3;
-    } else if (_tier == 1) {
-        whitelist[_userAddrs] -= _tier;
-        whitelist[_userAddrs] = 1;
-    } else if (_tier > 0 && _tier < 3) {
-        whitelist[_userAddrs] -= _tier;
-        whitelist[_userAddrs] = 2;
-    }
-    emit AddedToWhitelist(_userAddrs, _tier);
 }
-
-function whiteTransfer(address _recipient, uint256 _amount) public {
-    require(
-        whitelist[msg.sender] > 0 || whitelist[msg.sender] < 4,
-        "Gas Contract CheckIfWhiteListed modifier : revert happened because the user is not whitelisted"
-    );
-    require(
-        balances[msg.sender] >= _amount,
-        "Gas Contract - whiteTransfers function - Sender has insufficient Balance"
-    );
-    require(
-        _amount > 3,
-        "Gas Contract - whiteTransfers function - amount to send have to be bigger than 3"
-    );
-    balances[msg.sender] -= _amount;
-    balances[_recipient] += _amount;
-    balances[msg.sender] += whitelist[msg.sender];
-    balances[_recipient] -= whitelist[msg.sender];
-
-    emit WhiteListTransfer(_recipient);
-}
-}
-
